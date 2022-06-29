9cb2144c4f007e36551b4732da2880cd3656f9a4
Anthony Mattera <contact@kernelzechs.com>
2022-06-28 23:06:55 -0400

    Example Fix

--- build.php
+++ build.php
@@ -1,6 +1,6 @@
 <?php
 
-    $json = file_get_contents('test.json');
+    $json = file_get_contents('pathbuilder-export.json');
     require_once('classes/player.class.php');
     require_once('services/conversion.service.php');
 
